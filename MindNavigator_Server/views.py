c416df12ac1288c393e3a2fcc62f3e6073c8f3e1
Qobiljon <qobiljon.toshnazarov@gmail.com>
2018-06-22 08:42:57 +0900

    test for bugs

--- MindNavigator_Server/views.py
+++ MindNavigator_Server/views.py
@@ -1,6 +1,6 @@
 import json
-import datetime
-
+import calendar as cal
+import datetime as dt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response as Res
 from MindNavigator_Server.models import User, Event, Intervention, InterventionManager, Evaluation
@@ -23,16 +23,16 @@ def is_user_valid(username, password):
 
 
 def time_add(_time, _add):
-    time = datetime.datetime(
+    time = dt.datetime(
         year=int(_time / 1000000),
         month=int(_time / 10000) % 100,
         day=int(_time / 100) % 100,
         hour=int(_time % 100)
     )
     if _add >= 0:
-        time += datetime.timedelta(hours=int(_add / 60))
+        time += dt.timedelta(hours=int(_add / 60))
     else:
-        time -= datetime.timedelta(hours=int(-_add / 60))
+        time -= dt.timedelta(hours=int(-_add / 60))
     return int("%02d%02d%02d%02d" % (time.year % 100, time.month, time.day, time.hour))
 
 
@@ -54,6 +54,15 @@ def overlaps(user, start_time, end_time, except_id=None):
         elif Event.objects.filter(~Q(eventId=except_id), owner=user, startTime__lte=start_time,
                                   endTime__gte=end_time).exists():
             return True
+
+
+def weekday(millis):
+    temp = dt.datetime.fromtimestamp(millis / 1000.0)
+    return cal.weekday(year=temp.year, month=temp.month, day=temp.day)
+
+
+def add_timedelta(millis, timedelta):
+    return int(round((dt.datetime.fromtimestamp(millis / 1000.0) + timedelta).timestamp() * 1000))
 
 
 @api_view(['POST'])
@@ -90,28 +99,114 @@ def handle_login(request):
 def handle_event_create(request):
     json_body = json.loads(request.body.decode('utf-8'))
     if 'username' in json_body and 'password' in json_body and 'eventId' in json_body and 'title' in json_body and 'stressLevel' in json_body and 'startTime' in json_body and 'endTime' in json_body and 'intervention' in json_body \
-            and 'interventionReminder' in json_body and 'stressType' in json_body and 'stressCause' in json_body \
-            and 'repeatId' in json_body and 'repeatMode' in json_body and 'eventReminder' in json_body:
-        if is_user_valid(json_body['username'], json_body['password']) and not Event.objects.filter(
-                eventId=json_body['eventId']).exists() \
-                and not overlaps(User.objects.get(username=json_body['username']), start_time=json_body['startTime'],
-                                 end_time=json_body['endTime']):
-            Event.objects.create_event(
-                event_id=json_body['eventId'],
-                owner=User.objects.get(username=json_body['username']),
-                title=json_body['title'],
-                stress_level=json_body['stressLevel'],
-                start_time=json_body['startTime'],
-                end_time=json_body['endTime'],
-                intervention=json_body['intervention'],
-                interv_reminder=json_body['interventionReminder'],
-                stress_type=json_body['stressType'],
-                stress_cause=json_body['stressCause'],
-                repeat_mode=json_body['repeatMode'],
-                repeat_id=json_body['repeatId'],
-                event_reminder=json_body['eventReminder']
-            ).save()
-            return Res(data={'result': RES_SUCCESS})
+            and 'interventionReminder' in json_body and 'stressType' in json_body and 'stressCause' in json_body and 'repeatId' in json_body and 'repeatTill' in json_body and 'repeatMode' in json_body and 'eventReminder' in json_body:
+        if is_user_valid(json_body['username'], json_body['password']) and not Event.objects.filter(eventId=json_body['eventId']).exists():
+            user = User.objects.get(username=json_body['username'])
+            if json_body['repeatMode'] is Event.NO_REPEAT:
+                if not overlaps(User.objects.get(username=json_body['username']), start_time=json_body['startTime'], end_time=json_body['endTime']):
+                    # create a single event
+                    Event.objects.create_event(
+                        event_id=json_body['eventId'],
+                        owner=User.objects.get(username=json_body['username']),
+                        title=json_body['title'],
+                        stress_level=json_body['stressLevel'],
+                        start_time=json_body['startTime'],
+                        end_time=json_body['endTime'],
+                        intervention=json_body['intervention'],
+                        interv_reminder=json_body['interventionReminder'],
+                        stress_type=json_body['stressType'],
+                        stress_cause=json_body['stressCause'],
+                        repeat_mode=json_body['repeatMode'],
+                        repeat_id=json_body['repeatId'],
+                        event_reminder=json_body['eventReminder']
+                    ).save()
+                    return Res(data={'result': RES_SUCCESS})
+                else:
+                    return Res(data={'result': RES_FAILURE})
+            elif json_body['repeatMode'] is Event.REPEAT_EVERYDAY:
+                event_id = json_body['eventId']
+                start_time = json_body['startTime']
+                end_time = json_body['endTime']
+
+                if end_time - start_time > 86400000:
+                    return Res(data={'result': RES_FAILURE, 'reason': 'event length is longer than a day'})
+
+                while start_time < json_body['repeatTill']:
+                    if not overlaps(user=user, start_time=start_time, end_time=end_time):
+                        Event.objects.create_event(
+                            event_id=event_id,
+                            owner=User.objects.get(username=json_body['username']),
+                            title=json_body['title'],
+                            stress_level=json_body['stressLevel'],
+                            start_time=start_time,
+                            end_time=end_time,
+                            intervention=json_body['intervention'],
+                            interv_reminder=json_body['interventionReminder'],
+                            stress_type=json_body['stressType'],
+                            stress_cause=json_body['stressCause'],
+                            repeat_mode=json_body['repeatMode'],
+                            repeat_id=json_body['repeatId'],
+                            event_reminder=json_body['eventReminder']
+                        ).save()
+                    start_time += 86400000
+                    end_time += 86400000
+                    event_id += 1
+
+                return Res(data={'result': RES_SUCCESS})
+            elif json_body['repeatMode'] is Event.REPEAT_WEEKLY:
+                # create multiple events
+                start = [
+                    json_body['startTime'] if json_body['mon'] else None,
+                    json_body['startTime'] if json_body['tue'] else None,
+                    json_body['startTime'] if json_body['wed'] else None,
+                    json_body['startTime'] if json_body['thu'] else None,
+                    json_body['startTime'] if json_body['fri'] else None,
+                    json_body['startTime'] if json_body['sat'] else None,
+                    json_body['startTime'] if json_body['sun'] else None
+                ]
+                end = [
+                    json_body['endTime'] if json_body['mon'] else None,
+                    json_body['endTime'] if json_body['tue'] else None,
+                    json_body['endTime'] if json_body['wed'] else None,
+                    json_body['endTime'] if json_body['thu'] else None,
+                    json_body['endTime'] if json_body['fri'] else None,
+                    json_body['endTime'] if json_body['sat'] else None,
+                    json_body['endTime'] if json_body['sun'] else None
+                ]
+
+                if end[0] - start[0] > 604800000:
+                    return Res(data={'result': RES_FAILURE, 'reason': 'event length is longer than a week'})
+
+                event_id = json_body['eventId']
+                for x in range(7):
+                    if start[x] is None:
+                        continue
+                    day_delta = (7 - (weekday(start[x]) - x)) % 7
+                    start[x] = add_timedelta(start[x], dt.timedelta(days=day_delta))
+                    end[x] = add_timedelta(end[x], dt.timedelta(days=day_delta))
+
+                    while start[x] < json_body['repeatTill']:
+                        if not overlaps(user=user, start_time=start[x], end_time=end[x]):
+                            Event.objects.create_event(
+                                event_id=event_id,
+                                owner=User.objects.get(username=json_body['username']),
+                                title=json_body['title'],
+                                stress_level=json_body['stressLevel'],
+                                start_time=start[x],
+                                end_time=end[x],
+                                intervention=json_body['intervention'],
+                                interv_reminder=json_body['interventionReminder'],
+                                stress_type=json_body['stressType'],
+                                stress_cause=json_body['stressCause'],
+                                repeat_mode=json_body['repeatMode'],
+                                repeat_id=json_body['repeatId'],
+                                event_reminder=json_body['eventReminder']
+                            ).save()
+                        start[x] += 604800000
+                        end[x] += 604800000
+                        event_id += 1
+
+                return Res(data={'result': RES_SUCCESS})
         else:
             return Res(data={'result': RES_FAILURE})
     else:

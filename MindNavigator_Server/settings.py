42a65ade65de29489e6f2f83c25f5a3ceee1e909
Qobiljon <qobiljon.toshnazarov@gmail.com>
2018-06-30 15:41:29 +0900

    test for bugs

--- MindNavigator_Server/settings.py
+++ MindNavigator_Server/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = '4fyyo2&3%5qn00_0^8!%b)zi*v77-n3rjqlr7kc3bj0fhr0^ps'
 DEBUG = True
 
 ALLOWED_HOSTS = [
+    "127.0.0.1",
     "165.246.43.97",
     "165.246.43.162",
     u'qobiljon.pythonanywhere.com'
